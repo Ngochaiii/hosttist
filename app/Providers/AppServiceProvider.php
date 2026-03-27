0e7549b0447277fb9a3da1466444c0089d922146
Ngochaiii <phamngochaivphp@gmail.com>
2026-03-27 14:11:59 +0700

    hai update appSẻvicesProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
 
         View::composer('layouts.app', function ($view) {
