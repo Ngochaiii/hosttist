7386e01afbb003e7f0a9479f91ddece75bb006ce
Ngochaiii <phamngochaivphp@gmail.com>
2026-03-27 14:06:32 +0700

    hai update layout payment

--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
     <style>
         body {
             background-color: #f8f9fa;
@@ -56,7 +56,7 @@
     </div>
     @include('layouts.admin.layouts.footer')
     @include('layouts.admin.layouts.footer_js')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> --}}
     @stack('js')
 </body>
 
